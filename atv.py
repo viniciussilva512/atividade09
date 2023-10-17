de56c3e19a3c2a0686cd77c63653fd0ef62c0c37
viniciussilva512 <marvosvinicius128@gmail.com>
2023-10-17 16:21:41 -0300

    adicionei atividade09

---
+++ atv.py
@@ -0,0 +1,8 @@
+#exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+
+num1 = int(input("digite o primeiro número"))
+num2 = int(input("digite o segundo número"))
+if num1 > num2:
+    print (num1)
+else:
+    print(num2)
